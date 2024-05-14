5d538b0c5dc5972c01eba5ebe624ba3591ee8462
AB <markelofaleksei@gmail.com>
2024-05-14 16:41:19 +0300

    Fix

--- app/src/main/java/com/abshka/colorchanger/MainActivity.kt
+++ app/src/main/java/com/abshka/colorchanger/MainActivity.kt
@@ -1,6 +1,5 @@
 package com.abshka.colorchanger
 
-import android.annotation.SuppressLint
 import android.graphics.Color
 import android.os.Bundle
 import android.view.View
@@ -9,7 +8,6 @@ import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
 
-    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
